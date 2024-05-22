cd84669dc3f81ceaf16b11512f73ad854aa2ffd4
Pijush Ghorai <pghorai098@gmail.com>
2024-05-22 13:00:04 +0530

    first commit

---
+++ 01myfirstapp/src/App.js
@@ -0,0 +1,12 @@
+import Mymessage from "./message";
+
+function App() {
+  return (
+    <>
+    <h1>Chai aur React | Pijush Ghorai</h1>
+    <Mymessage />
+    </>
+  );
+}
+
+export default App;
---
+++ 01myfirstapp/src/message.jsx
@@ -0,0 +1,7 @@
+function Mymessage(){
+    return(
+        <p>Today is 19th May I started to learning React JS, The first thing I learn today create new project in React and it is so interesting</p>
+    )
+}
+
+export default Mymessage;
